623185228ee29b78089580c16952c98940544872
jaab30 <jaab30@yahoo.com>
2021-03-29 12:16:23 -0400

    production env fix for sequelize

--- config/config.js
+++ config/config.js
@@ -21,7 +21,11 @@ module.exports = {
 
     },
     production: {
-        use_env_variable: "JAWSDB_URL",
-        dialect: "mysql"
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        operatorsAliases: 0
     }
 }
